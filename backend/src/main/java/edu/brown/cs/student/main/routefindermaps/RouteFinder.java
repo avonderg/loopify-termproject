f4bfecad45473745a19b78e6a0446c65879b899c
Jose Urruticoechea Hurtado <jose_urruticoechea@brown.edu>
2022-05-03 11:49:30 -0400

    simple path algorithm

--- backend/src/main/java/edu/brown/cs/student/main/routefindermaps/RouteFinder.java
+++ backend/src/main/java/edu/brown/cs/student/main/routefindermaps/RouteFinder.java
@@ -12,19 +12,41 @@ import java.util.List;
 public class RouteFinder {
   private final int nodeNum = 5; // number of nodes per row / column
   Node start;
-  NodeGrid nodes;
+  NodeGrid nodeGrid;
   double distance;
 
   public RouteFinder(double startLat, double startLong, double distance) {
     this.distance = distance;
     //this.start = new Node(startLat, startLong, nodeNum/2, nodeNum/2);
-    nodes = new NodeGrid(startLat, startLong, distance, nodeNum);
+    this.nodeGrid = new NodeGrid(startLat, startLong, distance, nodeNum);
+    this.start = nodeGrid.getStartNode();
   }
 
   public List<Node> findRoute(){
     List<Node> route = new ArrayList<>();
-
+    List<Node> nodes = nodeGrid.getNodes();
+    double closestDist = 0;
+    Node node1;
+    Node node2;
+    Node pathNode1 = start;
+    Node pathNode2 = start;
+    for (int i = 0; i < nodes.size(); i++){
+      node1 = nodes.get(i);
+      for (int j = 0; j < nodes.size(); j++){
+        node2 = nodes.get(j);
+        if (i == j || i == start.getId() || j == start.getId()) continue;
+        double dist = nodeGrid.travelDistance(start, node1)
+                + nodeGrid.travelDistance(node1, node2)
+                + nodeGrid.travelDistance(node2, start);
+        if (closestDist >= dist){
+          closestDist = dist;
+          pathNode1 = node1;
+          pathNode2 = node2;
+        }
+      }
+    }
+    route.add(pathNode1);
+    route.add(pathNode2);
     return route;
   }
-
 }
